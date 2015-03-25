21d750839aeab2c4fc9fcec1e70f94af69b4fab9
zofiaOS <zofiaostojastarzycka@gmail.com>
2015-03-25 17:20:08 +0100

    CFY-2305 Code refactor

--- scripts/append_diamond_conf.py
+++ scripts/append_diamond_conf.py
@@ -2,23 +2,23 @@ from cloudify import ctx
 from cloudify.state import ctx_parameters as inputs
 
 
-targetInstance = ctx.target.instance
-srcInstance = ctx.source.instance
+target_instance = ctx.target.instance
+src_instance = ctx.source.instance
 
-config = srcInstance.runtime_properties.get('snmp_collector_config', {})
+config = src_instance.runtime_properties.get('snmp_collector_config', {})
 
-devicesConf = config.get('devices', {})
-devicesConf[ctx.target.node.name] = device_config = {}
-device_config['instance_id'] = targetInstance.id
+devices_conf = config.get('devices', {})
+devices_conf[ctx.target.node.name] = device_config = {}
+device_config['instance_id'] = target_instance.id
 if 'host' in inputs:
     device_config['host'] = inputs.host
 else:
-    device_config['host'] = targetInstance.host_ip
+    device_config['host'] = target_instance.host_ip
 device_config['port'] = inputs.port
 device_config['community'] = inputs.community
 device_config['oids'] = inputs.oids
 
-config['devices'] = devicesConf
+config['devices'] = devices_conf
 config['path_suffix'] = inputs.path_suffix
 
-srcInstance.runtime_properties['snmp_collector_config'] = config
+src_instance.runtime_properties['snmp_collector_config'] = config
