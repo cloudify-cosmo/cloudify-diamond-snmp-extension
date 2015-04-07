b73824957761d09c2fa0835fefe92896620710fe
zofiaOS <zofiaostojastarzycka@gmail.com>
2015-04-07 11:57:13 +0200

    CFY-2305 Changed test's superclass and removed unnecessary assertion

--- sys_tests/snmp_proxy.py
+++ sys_tests/snmp_proxy.py
@@ -1,9 +1,11 @@
-from cosmo_tester.test_suites.test_blueprints import monitoring_test
-from influxdb import InfluxDBClient
 from os import path
 
+from influxdb import InfluxDBClient
 
-class TestSNMPProxy(monitoring_test.MonitoringTest):
+from cosmo_tester.framework.testenv import TestCase
+
+
+class TestSNMPProxy(TestCase):
 
     def _check_snmp_monitoring(self, blueprint, inputs):
         self.blueprint_yaml = path.join(
@@ -13,15 +15,11 @@ class TestSNMPProxy(monitoring_test.MonitoringTest):
 
         self.upload_deploy_and_execute_install(inputs=inputs)
 
-        url = "http://{0}/#/deployment/{1}/monitoring" \
-              .format(self.env.management_ip, self.test_id)
-        self.assert_grafana_path_active(url)
-
         # Check that proper metrics are stored
         client = InfluxDBClient(
             self.env.management_ip, 8086, 'root', 'root', 'cloudify')
         all_series = client.get_list_series()
-        self.assertTrue(all_series) # not empty
+        self.assertTrue(all_series)  # not empty
         for s in all_series:
             # All series should refer to the snmp_monitore_host
             self.assertIn('snmp_monitored_host', s)
@@ -32,11 +30,11 @@ class TestSNMPProxy(monitoring_test.MonitoringTest):
         self.cfy.delete_deployment(self.test_id)
         self.cfy.delete_blueprint(self.test_id)
 
-    def test_monitoring(self, blueprint='proxy_on_manager.yaml'):
+    def test_snmp_proxy_on_manager(self):
         inputs = {
             'monitored_host_ubuntu_image_name': self.env.ubuntu_trusty_image_id
         }
-        self._check_snmp_monitoring(blueprint, inputs)
+        self._check_snmp_monitoring('proxy_on_manager.yaml', inputs)
 
     def test_snmp_proxy_on_separate_vm(self):
         inputs = {
